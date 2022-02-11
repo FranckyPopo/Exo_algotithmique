471457e6951c839e2c1c8223bcd37a0d41f4307e
FranckyPopo <afrifranck2003@gmail.com>
2022-02-11 16:27:03 +0000

    Les widgets Entry s'affiche dans les frames même fenêtre

--- BD.py
+++ BD.py
@@ -1,16 +1,30 @@
 import sqlite3
 import rsa
+import tkinter
+from tkinter import messagebox
+ 
+conn = sqlite3.connect("database.db")
+cursor = conn.cursor()
 
-def recording():
-    last_name = input("veuillez entrer vôtre nom: ")
-    firs_name = input("veuillez entrer vôtre prénom: ")
-    email = input("veuillez entrer vôtre email: ")
-    password = input("veuillez entrer vôtre mot de passe: ")
-    password_2 = input("veuillez confirmer le mot de passe: ")
+def display_recording():
+    frame_main.place_forget()
+    frame_recording.place(x=150, y=80)
+
+     
+def check_recording():
+    frame_main.place_forget()
+    
+    last_name = enter_last_name.get()
+    firs_name = enter_firs_name.get()
+    email = enter_email.get()
+    password = enter_password.get()
+    password_2 = enter_password_2.get()
     
     if last_name and firs_name and email and (password == password_2):
         # Le programme crype le mot de passe
         priv_key, pub_key = rsa.newkeys(512)
+        print(priv_key)
+        print(pub_key)
         password_encrypt = rsa.encrypt(password_2.encode(), pub_key)
         
         data_user = {
@@ -21,9 +35,6 @@ def recording():
         }
         
         try:
-            connection = sqlite3.connect("database.db")
-            cursor = connection.cursor()
-            
             cursor.execute("""
             CREATE TABLE IF NOT EXISTS users (
                 last_name text,
@@ -35,13 +46,94 @@ def recording():
             )
             cursor.execute("INSERT INTO users VALUES (:last_name, :firs_name, :email, :password)", data_user)
             
-            connection.commit()
-            connection.close()
+            conn.commit()
+            conn.close()
         except sqlite3.Error:
             print("Une erreur est survenu dans le scripte SQL")
         else:
-            print("Fin")
+            messagebox.showinfo("Inscription", "Vous venez de crée un compte")
+            
     else:
-        print("Impossible de crée vôtre compte veuillez verifier vos différentes informations")
+        label_error["fg"] = "red"
+
     
-recording()
+def connection():
+    try:
+        get_data = cursor.execute("SELECT * FROM users")
+        data_users = get_data.fetchall()
+        
+        conn.commit()
+        conn.close()
+    except sqlite3.Error:
+        print("Une erreur est survenu dans le scripte SQL")
+    else:
+        email = input("Veuillez entrer vôtre email: ")
+        password = input("Veuillez entrer vôtre mot de passe: ")
+        
+        for user in data_users:
+            if user[2] == email and user[3] == password:
+                print("compte existe")
+                break
+        else:
+            print("Le compte n'existe pas !")
+
+
+def back_recording():
+    frame_recording.place_forget()
+    frame_main.place(x=250, y=150)
+    
+    
+window = tkinter.Tk()
+window.geometry("720x480")
+window.title("Popo Page")
+window.resizable(False, False)
+
+# Frame principale
+frame_main = tkinter.Frame(window)
+frame_main.place(x=250, y=150)
+
+bnt_recording = tkinter.Button(frame_main, text="Inscription", relief="flat", command=display_recording)
+bnt_recording.grid(row=0, column=0, ipadx=10, ipady=5)
+
+bnt_recording = tkinter.Button(frame_main, text="Connection", relief="flat", command=display_recording)
+bnt_recording.grid(row=1, column=0, ipadx=10, ipady=5)
+
+# Frame d'inscription
+frame_recording = tkinter.Frame(window)
+
+label_last_name = tkinter.Label(frame_recording, text="Veuillez entrer vôtre addresse nom")
+label_last_name.grid(row=0, column=0, sticky="w")
+enter_last_name = tkinter.Entry(frame_recording, bg="white")
+enter_last_name.grid(row=1, column=0, sticky="w")
+
+label_firs_name = tkinter.Label(frame_recording, text="Veuillez entrer vôtre addresse prénom")
+label_firs_name.grid(row=2, column=0, sticky="w")
+enter_firs_name = tkinter.Entry(frame_recording)
+enter_firs_name.grid(row=3, column=0, sticky="w")
+
+label_email = tkinter.Label(frame_recording, text="Veuillez entrer vôtre addresse email")
+label_email.grid(row=4, column=0, sticky="w")
+enter_email = tkinter.Entry(frame_recording)
+enter_email.grid(row=5, column=0, sticky="w")
+
+label_password = tkinter.Label(frame_recording, text="Veuillez entrer vôtre mot de passe")
+label_password.grid(row=7, column=0, sticky="w")
+enter_password = tkinter.Entry(frame_recording)
+enter_password.grid(row=8, column=0, sticky="w")
+
+label_password_2 = tkinter.Label(frame_recording, text="Veuillez confirmer le mot de passe")
+label_password_2.grid(row=9, column=0, sticky="w")
+enter_password_2 = tkinter.Entry(frame_recording)
+enter_password_2.grid(row=10, column=0, sticky="w")
+
+label_error = tkinter.Label(frame_recording, text="Veuillez remplir tout les champ")
+label_error.grid(row=11, column=0, sticky="w")
+
+bnt_recording = tkinter.Button(frame_recording, text="S'inscrire", command=check_recording)
+bnt_recording.grid(row=12, column=0, sticky="w", ipadx=3, ipady=2)
+
+bnt_back = tkinter.Button(frame_recording, text="Retour", command=back_recording)
+bnt_back.grid(row=13, column=0, sticky="w", ipadx=3, ipady=2)
+
+
+window.mainloop()
