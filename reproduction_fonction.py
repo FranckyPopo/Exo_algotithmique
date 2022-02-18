a15ab1cf186d291575035f120054b0d82be9f448
FranckyPopo <afrifranck2003@gmail.com>
2022-02-18 09:44:25 +0000

    Amélioration de la fonction range

--- reproduction_fonction.py
+++ reproduction_fonction.py
@@ -1,10 +1,24 @@
-def functionn_range(first: int, end: int, step: int = 0) -> list:
+def function_range(first: int, end: int, step: int = 0) -> list:
     liste = list()    
     i = first  
     while i < end:
         liste.append(i)
-        print(i)
+        
+        if not step:
+            i += 1
+            continue
         i += step
     return liste
     
 
+def function_len(obj) -> int:
+    i = 0
+    for item in obj:
+        i += 1
+    return i
+
+# x = function_len(["hello word"])
+# print(x)
+
+x = function_range(0, 10, 2)
+print(x)
